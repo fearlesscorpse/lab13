e308717b36c0ee5e0cc24026bfc84a4ef2ffb014
fearlesscorpse <baranova.karina4@yandex.ru>
2026-04-08 14:05:36 +0300

    Выполнено задание 6, 7, 8

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     //Задание 1
     class Employee {
         public $name;
-        public $age;
+        private $age;
         public $salary;
         
         public function __construct($name, $age, $salary) {
@@ -16,18 +16,31 @@
         }
         //Задание 4
         public function getAge() {
-            return $this->age;
+          return $this->age;
         }
-        //Задание 5
-        public function getSalary() {
-            return $this->salary;        
+        //Задание 5 и 6
+        public function getSalary($emp2) {
+            return $this->salary + $emp2->salary;        
+        }
+        //Задание 7 и 8
+        public function setAge($newAge) {
+            if($newAge >= 18) {
+                $this->age = $newAge;
+            } else {
+                echo "Задание 8. Вам еще работать в нашей компании рано<br>";            
+            }
         }
     }
     $employee1 = new Employee("Карина", 18, 100000);
     $employee2 = new Employee("Вера", 19, 10000);
     //Задание 2
-    echo "Задание 2. Сумма зарплат: " . ($employee1->salary + $employee2->salary) . "   рублей" . " и сумма возрастов: " . ($employee1->age + $employee2->age) . " лет<br>";
+    $sumAge = $employee1->getAge() + $employee2->getAge();
+    echo "Задание 2. Сумма зарплат: " . ($employee1->salary + $employee2->salary) . "   рублей" . " и сумма возрастов: " . $sumAge . " лет<br>";
     //Задание 3,4,5
-    echo "Задание 3,4,5 (созданы методы и по нему выведено на экран). Имя - " . $employee1->getName() . " возраст - " . $employee1->getAge() . " зарплата - " . $employee1->getSalary() . "<br>"; 
-
+    echo "Задание 3,4,5 (созданы методы и по нему выведено на экран). Имя - " . $employee1->getName() . "<br>"; 
+    //Задание 6
+    echo "Задание 6. Сумма зарплат через метод getSalary() - " . $employee1->getSalary($employee2) . " рублей<br>";
+    //Задание 7
+    $employee2->setAge(20);
+    echo "Задание 7. Новый возраст работника2 (через setAge()) - " . $employee2->getAge() . " лет<br>"; 
 ?>
