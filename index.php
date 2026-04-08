7ff36189fd8831df6c6465df3a8334e9679e5074
fearlesscorpse <baranova.karina4@yandex.ru>
2026-04-08 13:43:26 +0300

    Выполнено задание 3, 4, 5

--- index.php
+++ index.php
@@ -10,10 +10,24 @@
             $this->age = $age;
             $this->salary = $salary;        
         }
+        //Задание 3
+        public function getName() {
+            return $this->name;        
+        }
+        //Задание 4
+        public function getAge() {
+            return $this->age;
+        }
+        //Задание 5
+        public function getSalary() {
+            return $this->salary;        
+        }
     }
     $employee1 = new Employee("Карина", 18, 100000);
     $employee2 = new Employee("Вера", 19, 10000);
     //Задание 2
-    echo "Задание 2. Сумма зарплат: " . ($employee1->salary + $employee2->salary) . " рублей" . " и сумма возрастов: " . ($employee1->age + $employee2->age) . " лет<br>";
+    echo "Задание 2. Сумма зарплат: " . ($employee1->salary + $employee2->salary) . "   рублей" . " и сумма возрастов: " . ($employee1->age + $employee2->age) . " лет<br>";
+    //Задание 3,4,5
+    echo "Задание 3,4,5 (созданы методы и по нему выведено на экран). Имя - " . $employee1->getName() . " возраст - " . $employee1->getAge() . " зарплата - " . $employee1->getSalary() . "<br>"; 
 
 ?>
